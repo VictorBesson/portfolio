2c769a4c98bc13e107ddc2391a236f2e8820aeb3
Victor <victor.besson@gmail.com>
2021-09-19 10:15:19 +0200

    add fetching of all projects in controller

--- src/Controller/ProjectsController.php
+++ src/Controller/ProjectsController.php
@@ -2,17 +2,21 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\ProjectRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ProjectsController extends AbstractController
 {
     #[Route('/projects', name: 'projects')]
-    public function index(): Response
+    public function index(ProjectRepository $projectRepository): Response
     {
+        $projects = $projectRepository->findAll();
+
         return $this->render('projects/index.html.twig', [
             'controller_name' => 'ProjectController',
+            'projects' => $projects,
         ]);
     }
 }
