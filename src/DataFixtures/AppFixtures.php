3e3fb398949e448ca14b0e8b12dd49adf442534b
Victor <victor.besson@gmail.com>
2021-09-19 10:01:33 +0200

    adding fixture and fake data for Project

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use App\Entity\Project;
+use Faker;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        $faker = Faker\Factory::create();
+        for ($i = 0; $i <= 5; $i++) {
+
+            $project = new Project();
+            $project->setName($faker->words(2, true));
+            $project->setDescription($faker->paragraph());
+            $project->setLink($faker->url());
+            $manager->persist($project);
+        }
+
+        $manager->flush();
+    }
+}
